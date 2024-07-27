357238b4912180f92ed4764573d31aaa2b948379
EpikGoose <maximdon2010@gmail.com>
2024-07-26 21:49:32 -0700

    MadLibs

---
+++ MadLibs.py
@@ -0,0 +1,5 @@
+# use the 2 variables to store the two nouns.
+answerOne = input("Enter a noun:")
+answerTwo = input("Enter another noun:")
+# Fun fact: Since it was so late at night, I completely ignored the language that I was using and typed out line 5 as console.log instead of print lol.
+print(f"What came first? The {answerOne} or the {answerTwo}?")
